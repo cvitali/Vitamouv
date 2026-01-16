621cbda4332a7b34e67d21eee9630ac06162f53a
CorinDesbois <corinne.wilwert@yahoo.fr>
2026-01-16 08:21:50 +0100

    Finalisation de la page Mini'MOUV.

---
+++ Vitamouv/Controllers/ActivitiesController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vitamouv.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        public IActionResult Dansemouv()
+        {
+            return View();
+        }
+
+        public IActionResult Eventmouv()
+        {
+            return View();
+        }
+
+        public IActionResult Minimouv()
+        {
+            return View();
+        }
+        public IActionResult Schoolmouv()
+        {
+            return View();
+        }
+        public IActionResult Slowmouv()
+        {
+            return View();
+        }
+
+    }
+}
--- Vitamouv/Controllers/HomeController.cs
+++ Vitamouv/Controllers/HomeController.cs
@@ -23,6 +23,16 @@ namespace Vitamouv.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Minimouv()
+        {             
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
