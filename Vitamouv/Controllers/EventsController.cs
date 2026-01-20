f6fe09581c88932c780027d582b5c1716da46423
CorinDesbois <corinne.wilwert@yahoo.fr>
2026-01-20 15:04:02 +0100

    Finalisation de la page eventmouv + ajout du code js permettant de naviguer grâce au logo interactif

---
+++ Vitamouv/Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vitamouv.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Birthday()
+        {
+            return View();
+        }
+
+        public IActionResult EventsCreation()
+        {
+            return View();
+        }
+
+        public IActionResult Wedding()
+        {
+            return View();
+        }
+    }
+
+    
+}
--- Vitamouv/wwwroot/js/site.js
+++ Vitamouv/wwwroot/js/site.js
@@ -1,4 +1,28 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿//liens du logo interactif renvoyant vers les différentes disciplines
 
-// Write your JavaScript code.
+let links = document.querySelectorAll(".link-js")
+
+links.forEach((link) => {
+    link.addEventListener("click", () => {
+        if (link.classList.contains("rect__mini"))
+        {
+            location.href = "../Activities/Minimouv"
+        }
+        if (link.classList.contains("rect__school")) {
+            location.href = "../Activities/Schoolmouv"
+        }
+        if (link.classList.contains("rect__slow")) {
+            location.href = "../Activities/Slowmouv"
+        }
+        if (link.classList.contains("rect__danse")) {
+            location.href = "../Activities/Dansemouv"
+        }
+        if (link.classList.contains("rect__event")) {
+            location.href = "../Activities/Eventmouv"
+        }
+    })
+})
+
+
+//page Eventmouv: modification du texte sous les photos au survol de la souris
+
