c70f005a2d9afec4df5d7a21309954fbe0f46b57
CorinDesbois <corinne.wilwert@yahoo.fr>
2026-04-09 10:19:24 +0200

    Corrections de textes mineures  + configuration de HSTS dans program.cs

--- Vitamouv/Program.cs
+++ Vitamouv/Program.cs
@@ -16,6 +16,15 @@ builder.Services.AddControllersWithViews();
 //injection de dépendance pour le service de messagerie
 builder.Services.AddScoped<IEmailService, SendGridEmailService>();
 
+//configuration du HSTS pour les environnements de production
+builder.Services.AddHsts(options =>
+{
+    options.MaxAge = TimeSpan.FromDays(365); //HTTPS forcé pendant 1 an après la première visite par le navigateur
+    options.IncludeSubDomains = true; //Tous les sous - domaines inclus
+    options.Preload = true; //Ajout à HSTS Preload List pour que les navigateurs connaissent d'emblée la politique HSTS du site
+});
+
+
 var apiKey = builder.Configuration["SendGrid:ApiKey"];
 
 var app = builder.Build();
@@ -25,7 +34,7 @@ if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    app.UseHsts(); //Force le HTTPS en production, en envoyant des en-têtes HSTS aux navigateurs pour indiquer que le site doit être accédé uniquement via HTTPS.
 }
 
 app.UseHttpsRedirection();
